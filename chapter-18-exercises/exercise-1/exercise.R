ccf6696718ae9c92e2fbc4fbc9734ef87762e1dd
Timothy Roach <tmroach@uw.edu>
2020-02-14 22:00:40 -0800

    Complete week 6 exercises

--- chapter-18-exercises/exercise-1/exercise.R
+++ chapter-18-exercises/exercise-1/exercise.R
@@ -2,15 +2,21 @@
 library("httr")
 library("jsonlite")
 
+
+
 # Be sure and check the README.md for complete instructions!
 
 
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
 
+source("apikey.R")
+
+query_params <- list("api-key" = nyt_key)
 
 # Create a variable `movie_name` that is the name of a movie of your choice.
 
+movie_name <- "The Godfather"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -21,21 +27,53 @@ library("jsonlite")
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie_name` variable as the search query!
 
+base_uri <- "https://api.nytimes.com/svc/movies/v2/"
+
+resource <- "reviews/search.json"
+
+uri <- paste0(base_uri, resource)
+
+query_params <- list("api-key" = nyt_key, "movie_name" = movie_name)
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
 
+response <- GET(uri, query = query_params)
+body <- content(response, "text", encoding = "UTF-8")
 
 # What kind of data structure did this produce? A data frame? A list?
 
+is.data.frame(response)
+
+is.list(response)
+
+# It is in fact a list
 
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
 # Use functions such as `names()`, `str()`, etc.
 
+names(response)
+
+str(response)
 
 # Flatten the movie reviews content into a data structure called `reviews`
 
+response_content <- content(response, "text")
+
+response_text <- fromJSON(response_content)
+
+names(response_text)
+
+response$num_results
+
+response_text$status
+
+response$results
+
+is.data.frame(response_text)
+
+flatten(response_text)
 
 # From the most recent review, store the headline, short summary, and link to
 # the full article, each in their own variables
